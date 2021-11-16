28e68ac8bf57afd6be83f165c26a0101acbe0c0d
SteevenAlbert <Steven.albert2001@gmail.com>
2021-11-16 23:46:29 +0200

    Image Upload Added

---
+++ processImage.php
@@ -0,0 +1,23 @@
+<?php
+
+if(isset($_POST['submit'])){
+
+echo "<pre>", print_r($_FILES['profileImage']['name']), "</pre>"; 
+$profileImageName = time() .'_'.$_FILES['profileImage']['name'];
+$TempImageName = $_FILES['profileImage']['tmp_name'];
+
+$target='images/'. $profileImageName;
+$result = move_uploaded_file($TempImageName, $target);
+if($result){
+    echo "Image Uploaded Successfully";
+}else{
+    echo "Image Upload Failed";
+
+}
+
+
+}
+
+
+?>
+ 
