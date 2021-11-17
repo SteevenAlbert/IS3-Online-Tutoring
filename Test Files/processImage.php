832cafe896ff237028afa18ff1c8900d00b67508
SteevenAlbert <Steven.albert2001@gmail.com>
2021-11-17 15:44:36 +0200

    Test

--- Test Files/processImage.php
+++ Test Files/processImage.php
@@ -2,9 +2,13 @@
 
 if(isset($_POST['submit'])){
 
+
+    echo "<pre>", print_r($_FILES['profileImage']), "</pre>";   
 echo "<pre>", print_r($_FILES['profileImage']['name']), "</pre>"; 
 $profileImageName = time() .'_'.$_FILES['profileImage']['name'];
 $TempImageName = $_FILES['profileImage']['tmp_name'];
+
+
 
 $target='images/'. $profileImageName;
 $result = move_uploaded_file($TempImageName, $target);
