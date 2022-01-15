d232218fef6f1444e01a285d703bbb23656e81e5
judykhairalla <judykhairalla@gmail.com>
2022-01-15 03:02:46 +0200

    Cart Layout added

--- src/model/Cart/cart.php
+++ src/model/Cart/cart.php
@@ -13,6 +13,7 @@
 <!-- Fonts -->
 <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@600&display=swap" rel="stylesheet">
 
+<link rel="stylesheet" href="/IS3-Online-Tutoring/CSS/cart.css" type="text/css">
 
 <?php
  session_start();
@@ -24,7 +25,7 @@ isLearner();
 
 //------------------------------------ Get Cart courses ------------------------------------
 $user = $_SESSION['UserID'];
-$query1="SELECT c1.title, c2.CourseID FROM courses c1,cartcourses c2 WHERE c2.CourseID=c1.CourseID AND c2.UserID='$user'";
+$query1="SELECT c1.title, c1.code, c1.Categories, c1.Hours, c1.CreatedBy, c1.Thumbnail, c1.price, c2.CourseID FROM courses c1,cartcourses c2 WHERE c2.CourseID=c1.CourseID AND c2.UserID='$user'";
 $query2="SELECT SUM(price) FROM courses c1,cartcourses c2 WHERE c2.CourseID=c1.CourseID AND c2.UserID='$user'";
 $result1=mysqli_query($conn,$query1);
 $result2=mysqli_query($conn,$query2);
@@ -46,21 +47,50 @@ try{
 }catch(Exception $e){  
    echo"Message:", $e->getMessage();  
 }
+?>
 
-echo "<table border=1>     
-  <th>Course Title</th> 
-  <th>Remove from cart</th>";
+<div class = "page-content"> 
+    <div class="page-title">
+		<h1>Shopping Cart</h1>
+	</div>  
+    <div class = "col-lg-8"> 
+    <ul class = "courses-list">
+    <label> Courses </label>
 
+<?php
 //------------------------------------ Display Cart Courses ------------------------------------
 while($rows = $result1->fetch_array(MYSQLI_ASSOC)){
+    $target = "/IS3-Online-Tutoring/resources/CoursesContent/Thumbnails/".$rows['Thumbnail'];
+
     ?>
-    <tr>
-    <td><?php echo$rows["title"]?></td>
-    <td><a href=cart.php?id=<?php echo$rows["CourseID"]?>>Remove</a> </td>
-    </tr>
+    <li class = "row">
+        <div class="col-lg-4">
+            <img src=<?php echo $target?> height = 100>
+        </div>
+        <div class="col-lg-5">
+            <p class="text-muted"> <?php echo $rows['Categories']?> </p>
+            <label> <?php echo $rows['code'].$rows["title"]?> </label>
+            <p class="text-muted"> <?php echo getUsername($rows["CreatedBy"]);?> </p>
+            <p class="text-muted"> <?php echo $rows["Hours"]." hours"?> </p>
+            <?php 
+            getRating($rows, $averageRating, $reviewCount);
+            displayRating($averageRating); 
+            ?>
+            
+        </div>
+        <label class = 'col-lg-2'> EGP <?php echo $rows['price']?> </label>
+        <a class="col-lg-1" href=cart.php?id=<?php echo$rows["CourseID"]?>><i class="fas fa-times"></i></a>
+    </li>
  <?php
 }
+?>
 
+</ul>
+</div>
+
+
+
+<?php
 //------------------------------------ Remove course from cart ------------------------------------
 function Remove($conn) {
     $get_id = $_GET["id"];
@@ -94,7 +124,8 @@ if (isset($_GET["id"])) {
 }
 
 //------------------------------------ Display total price ------------------------------------
-echo "<form action='/IS3-Online-Tutoring/src/actions/learner/enroll.php' method='post'>";
+echo "<div class='col-lg-2>'";
+echo "<form class = 'form-control'action='/IS3-Online-Tutoring/src/actions/learner/enroll.php' method='post'>";
 
 $numberOfCourses = mysqli_num_rows($result1);
 echo "<input hidden type = text name = 'amount' value = $numberOfCourses><br>";
@@ -102,11 +133,51 @@ echo "<input hidden type = text name = 'amount' value = $numberOfCourses><br>";
 
 while($rows = $result2->fetch_array(MYSQLI_ASSOC)){
     foreach($rows as $row)
-      echo "total <input type = text name = 'total' value =".$row." readonly><br>";
+    echo "<label> Total </label>";
+    echo "<div class = 'total-price'> EGP $row </div>";
+    echo "<input hidden type = text name = 'total' value =".$row." readonly><br>";
 }
 echo"<br>";
 
 //--------------------------------------- Purchase button ------------------------------------
 ?>
-    <input type="submit" value="Purchase" name="Submit1"><br/><br/>
+    <input type="submit" value="CHECKOUT" name="Submit1" class="btn btn-primary">
 </form>
+
+</div>
+</div>
+
+
+<?php
+function getRating($row, &$averageRating, &$reviewCount)
+{
+    $averageRating=5;
+    $reviewCount=0;
+
+    $getReviewsQuery = "SELECT * FROM ratings WHERE CourseID=".$row["CourseID"];
+    $reviews = $GLOBALS['conn']->query($getReviewsQuery);
+    
+    if (!$reviews)
+        die ("Query error. $getReviewsQuery");
+
+    $reviewsTotal=0;
+    while($review= $reviews->fetch_array(MYSQLI_ASSOC)){
+        $reviewCount +=1; 
+        $reviewsTotal+= $review['rating'];
+    }
+
+    if($reviewCount)
+        $averageRating = round($reviewsTotal / $reviewCount, 1);
+}
+
+function displayRating($rating)
+{
+    for ($i = 0; $i < 5; $i++)
+    {
+        if ($i <= $rating-1)
+            echo "<span class='fa fa-star checked'></span>";
+        else
+            echo "<span class='fa fa-star unchecked'></span>";
+    }
+}
+?>
